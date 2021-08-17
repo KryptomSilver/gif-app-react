7a6e0dc08276251084fa7e0881f46b7a3fa3728b
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-08-17 13:13:59 -0500

    Added PropTypes on component <GifGrid/>

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 import GifGridItem from "./GifGridItem";
+import PropTypes from "prop-types";
 
 const GifGrid = ({ category }) => {
   const { data: images, loading } = useFetchGifs(category);
@@ -16,5 +17,7 @@ const GifGrid = ({ category }) => {
     </>
   );
 };
-
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
+};
 export default GifGrid;
